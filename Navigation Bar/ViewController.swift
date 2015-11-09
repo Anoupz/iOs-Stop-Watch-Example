6a1dd8e2f45414c64c10ca6e1249f67ee0eee14f
Anoop Narayanan <anarayanan@prosper.com>
2015-11-08 22:07:06 -0800

    Initial Commit

---
+++ Navigation Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Bar
+//
+//  Created by Anoop Narayanan on 11/8/15.
+//  Copyright © 2015 APNApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
